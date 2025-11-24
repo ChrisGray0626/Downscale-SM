7e4f381dd17d7c2da0d1ff05570651e1f52c2b88
ChrsiGray <yuxiaohui0626@gmail.com>
2025-11-24 17:11:28 +0800

    feat(util/workflow): WorkflowConstant

---
+++ src/util/workflow/WorkflowConstant.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+  @Description Workflow Constant Definition
+  @Author Chris
+  @Date 2025/11/23
+"""
+
+# Control flow
+IS_SKIP_KEY = "is_skip"
+
+SRC_FILE_PATH_KEY = "src_file_path"
+SRC_FILE_PATHS_KEY = "src_file_paths"
+SRC_DIR_PATH_KEY = "src_dir_path"
+DST_FILE_PATH_KEY = "dst_file_path"
+DST_DIR_PATH_KEY = "dst_dir_path"
+
+RAW_DIR_PATH_KEY = "raw_dir_path"
+CONVERTED_DIR_PATH_KEY = "converted_dir_path"
+MERGED_DIR_PATH_KEY = "merged_dir_path"
+RESAMPLED_DIR_PATH_KEY = "resampled_dir_path"
+DECOMPRESSED_DIR_PATH_KEY = "decompressed_dir_path"
+
+# Resample
+REF_GRID_PATH_KEY = "ref_grid_path"
+RESOLUTION_CONFIGS_KEY = "resolution_configs"
+
+# Data
+DATA_KEY = "data"
+GAP_VALUE_KEY = "gap_value"
+SCALE_FACTOR_KEY = "scale_factor"
+TRANSFORM_KEY = "transform"
+PROJECTION_KEY = "projection"
+X_SIZE_KEY = "x_size"
+Y_SIZE_KEY = "y_size"
