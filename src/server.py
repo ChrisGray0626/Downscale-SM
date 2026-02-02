1542e8bd8ccf76f5895fbba17d12129fedb422e1
ChrsiGray <yuxiaohui0626@gmail.com>
2026-02-02 10:29:56 +0800

    feat(server)

---
+++ src/server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+  @Description Server Entry Point
+  @Author Chris
+  @Date 2026/2/2
+"""
+import resnet.resnet_inferencer
+
+
+def main():
+    resnet.resnet_inferencer.main()
+
+
+if __name__ == "__main__":
+    main()
